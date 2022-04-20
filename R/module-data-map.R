60fea34e68a370f5a8b39c997a3e2d4f0477bf0c
pvictor <victor.perrier@dreamRs.fr>
2022-04-20 16:03:48 +0200

    updated map to use cluster markers

--- R/module-data-map.R
+++ R/module-data-map.R
@@ -63,25 +63,33 @@ data_map_server <- function(id, data_r = reactive(NULL)) {
       data_rv <- reactiveValues(init = NULL, select = NULL, ts = NULL)
 
       observeEvent(data_r(), {
+        # datamap <- req(data_r()) %>%
+        #   dplyr::group_by(.__latitude, .__longitude) %>%
+        #   dplyr::summarise(n = dplyr::n(), id = dplyr::cur_group_id())
         datamap <- req(data_r()) %>%
-          dplyr::group_by(.__latitude, .__longitude) %>%
-          dplyr::summarise(n = dplyr::n(), id = dplyr::cur_group_id())
+          dplyr::mutate(id = seq_len(dplyr::n()))
         pts_sf <- sf::st_as_sf(datamap, coords = c(".__latitude", ".__longitude"))
         pts_sf <- crosstalk::SharedData$new(pts_sf, key = ~id)
         data_rv$init <- data_rv$select <- pts_sf
       })
 
+
+
       output$map <- renderLeaflet({
-        if (is.null(data_rv$init)) {
-          leaflet() %>%
-            addTiles()
-        } else {
-          leaflet(data_rv$init) %>%
-            addTiles() %>%
-            addMarkers(
-              popup = ~paste("Number of records: ", n)
-            )
+        basemap <- leaflet::leaflet(data = data_rv$init) %>%
+          leaflet::addProviderTiles(leaflet::providers$OpenStreetMap, group = "OSM") %>%
+          leaflet::addProviderTiles(leaflet::providers$Esri.WorldImagery, group = "Esri") %>%
+          leaflet::addProviderTiles(leaflet::providers$OpenTopoMap, group = "Open Topo Map") %>%
+          leaflet::addLayersControl(
+            baseGroups = c("OSM", "Esri", "Open Topo Map"),
+            options = leaflet::layersControlOptions(collapsed = FALSE)
+          )
+        if (!is.null(data_rv$init)) {
+          basemap <- basemap %>%
+            leaflet::addCircleMarkers(fillOpacity = 0, opacity = 0) %>%
+            leaflet::addMarkers(clusterOptions = leaflet::markerClusterOptions())
         }
+        basemap
       })
 
       observe({
@@ -107,10 +115,10 @@ data_map_server <- function(id, data_r = reactive(NULL)) {
         req(data_rv$init)
         data_rv$select <- data_rv$init
         leafletProxy("map", data = data_rv$select) %>%
-          clearMarkers() %>%
-          addMarkers(
-            popup = ~paste("Number of records: ", n)
-          )
+          leaflet::clearMarkers() %>%
+          leaflet::clearMarkerClusters() %>%
+          leaflet::addCircleMarkers(fillOpacity = 0, opacity = 0) %>%
+          leaflet::addMarkers(clusterOptions = leaflet::markerClusterOptions())
       })
 
       # Remove points
@@ -120,10 +128,10 @@ data_map_server <- function(id, data_r = reactive(NULL)) {
         new_data <- data_sel %>% dplyr::filter(selected_ == FALSE)
         data_rv$select <- crosstalk::SharedData$new(new_data, key = ~id)
         leafletProxy("map", data = data_rv$select) %>%
-          clearMarkers() %>%
-          addMarkers(
-            popup = ~paste("Number of records: ", n)
-          )
+          leaflet::clearMarkers() %>%
+          leaflet::clearMarkerClusters() %>%
+          leaflet::addCircleMarkers(fillOpacity = 0, opacity = 0) %>%
+          leaflet::addMarkers(clusterOptions = leaflet::markerClusterOptions())
       })
 
       return(reactive(NULL))
