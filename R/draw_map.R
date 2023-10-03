6235683a7bc974edbd29b3cbda72a6648383b2a9
pvictor <victor.perrier@dreamRs.fr>
2023-10-03 13:48:18 +0200

    use rnaturalearth without rnaturalearthdata

--- R/draw_map.R
+++ R/draw_map.R
@@ -10,34 +10,23 @@ draw_map <- function(.data, resolution = 10) {
     st_as_sf(coords = c(".__longitude", ".__latitude"))
   st_crs(data_latlon_sf) <- 4326
   data_latlon_sf <- st_transform(data_latlon_sf, "EPSG:6933")
-  
-  all_c <- rnaturalearth::ne_countries(scale = 50, returnclass = "sf")
-  bbox_gab <- all_c %>% filter(iso_a3 == "GAB")
-  # bbox_gab <- st_sf(
-  #   1,
-  #   geom = st_geometry(st_polygon(list(
-  #     matrix(
-  #       c(8,-4, 15,-4, 15, 2.5, 8, 2.5, 8,-4),
-  #       ncol = 2,
-  #       byrow = TRUE
-  #     )
-  #   )))
-  # )
-  
+
+  bbox_gab <- rnaturalearth::ne_countries(scale = 110, country = "Gabon", returnclass = "sf")
+
   st_crs(bbox_gab) <- 4326
   bbox_gab <- st_transform(bbox_gab, "EPSG:6933")
-  
+
   grid <- st_make_grid(
     x = bbox_gab,
     cellsize = resolution * 1000
   )
   grid <- st_as_sf(grid) %>%
     mutate(id_grid = dplyr::row_number())
-  
-  
+
+
   data_latlon_sf <- sf::st_intersection(data_latlon_sf, bbox_gab)
   intersect_grid <- st_intersection(data_latlon_sf, grid)
-  
+
   sampling_cell <-
     intersect_grid %>%
     group_by(id_grid) %>%
@@ -46,25 +35,25 @@ draw_map <- function(.data, resolution = 10) {
       n = length(.__taxa)
     ) %>%
     st_set_geometry(NULL)
-  
+
   grid_not_null <- grid %>%
     left_join(sampling_cell) %>%
     filter(!is.na(n))
-  
+
   grid_not_null <- st_transform(grid_not_null, 4326)
-  
+
   pal <- colorNumeric(
     palette = "Reds",
     domain = grid_not_null$nbe_esp
   )
-  
+
   # create_popup <- function(.data) {
   #   template <- glue::glue("<b>{column}:</b>", column = names(.data))
   #   template <- glue::glue("{template} {valeur}</br>", template = template, valeur = sprintf("{%s}", names(.data)))
   #   template <- paste(template, collapse = "")
   #   glue::glue_data(.data, template)
   # }
-  
+
   leaflet() %>%
     addProviderTiles(providers$OpenStreetMap, group = "OSM") %>%
     addProviderTiles(providers$Esri.WorldImagery, group = "Esri") %>%
@@ -81,7 +70,7 @@ draw_map <- function(.data, resolution = 10) {
       fill =  "black",
       popup = paste("Number of species: ", grid_not_null$nbe_esp),
       fillOpacity = 0.5,
-      highlight = leaflet::highlightOptions(
+      highlightOptions = leaflet::highlightOptions(
         color = "black",
         weight = 4
       )
